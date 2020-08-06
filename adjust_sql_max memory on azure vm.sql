7c37c2b792f426b33c899b9b981f6f2769defd3f
Brendan O'Connor <40264229+wittyslogan@users.noreply.github.com>
2020-08-06 14:11:01 +0100

    Add files via upload
    Have ever had someone adjust the add RAM to a SQL VM on AZURE?  If you are not told then the MAX memory on SQL will not change.  This script will allow you to adjust the MAX memory automatically.  
     Because Azure reboots when RAM size is adjusted the job is enabled to run when SQL restarts.  It checks the Physical memory of the Server and adjusts the MAX memory of the instance to a percentage of that.  If you have multiple instances you can put this job on each instance with a set percentage on each depending how much MAX memeory you want to allocate.

---
+++ adjust_sql_max memory on azure vm.sql
@@ -0,0 +1,92 @@
+USE [msdb]
+GO
+
+/****** Object:  Job [Adjust_max_memory_setting]    Script Date: 05/08/2020 16:23:26 ******/
+/****** This job will adjust max sql memory whenever the VM RAM size is changed.  Because Azure reboots when RAM size is adjusted the job is enabled to run when SQL restarts.  It checks the Physical memory of the Server and adjusts the MAX memory of the instance to a percentage of that.  If you have multiple instances you can put this job on each instance with a set percentage on each depending how much MAX memeory you want to allocate********/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 05/08/2020 16:23:26 ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Adjust_max_memory_setting', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [reset max memory]    Script Date: 05/08/2020 16:23:26 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'reset max memory', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'declare @reset_memory int
+ 
+
+EXEC sp_CONFIGURE 'show advanced options',1
+RECONFIGURE
+GO
+
+SELECT @reset_memory =([total_physical_memory_kb] / 1024 )*0.89
+
+ 
+
+
+FROM [master].[sys].[dm_os_sys_memory]
+
+
+
+EXEC sys.sp_configure N''max server memory (MB)'', @reset_memory;
+
+
+GO
+
+RECONFIGURE WITH OVERRIDE
+
+GO', 
+		@database_name=N'master', 
+		@flags=8
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'reset', 
+		@enabled=1, 
+		@freq_type=64, 
+		@freq_interval=0, 
+		@freq_subday_type=0, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20200803, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
+
+
